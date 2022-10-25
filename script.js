54da871ac5b6da5377c43c8a45e5128d97632d3d
euni-yi <ejky53@gmail.com>
2022-10-25 11:19:13 -0600

    odne

--- script.js
+++ script.js
@@ -8,13 +8,21 @@ document.getElementById("btn").addEventListener("click", btnClicked);
 
 function btnClicked(){
 
-let user = document.getElementById("user").value.toLowerCase;
-let pass = document.getElementById("pass").value.toLowerCase;
+let user = document.getElementById("user").value.toLowerCase();
+let pass = document.getElementById("pass").value;
+console.log(user);
+console.log(pass);
     // input
-    if(user==="admin" || pass === "1234" ){
-        console.log("alert succesful")
-        alert("Login Sucessful")
-    } else if(user === "admin"){
-        alert("Invalid Password")
-    }
+ if (user==="admin" && pass==="1234"){
+    console.log("hi")
+     alert("Login Successful")
+ }else if(user==="admin"){
+     alert("invalid password")
+}else if(pass==="1234"){
+    console.log("passwoerd correct")
+    alert("invalid username")
+}else{
+    alert("invalid passowrd amd usernsme")
+}
+
 }
